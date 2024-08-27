c54ffc14e1ef6faaae4162e7f99eed1cce635a69
Preet Patel <preetx.patel@mail.utoronto.ca>
2024-08-26 21:20:02 -0400

    Added small and big side bars for dashboard.

---
+++ client/src/components/SmallSideBar.jsx
@@ -0,0 +1,25 @@
+import Wrapper from '../assets/wrappers/SmallSidebar';
+import { FaTimes } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
+import { useDashboardContext } from '../pages/DashboardLayout';
+import Logo from './Logo';
+import NavLinks from './NavLinks';
+
+const SmallSideBar = () => {
+  const { showSidebar, toggleSidebar } = useDashboardContext();
+  
+  return <Wrapper>
+    <div className={showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'}>
+      <div className="content">
+        <button type='button' className='close-btn' onClick={toggleSidebar}>
+          <FaTimes />
+        </button>
+        <header>
+          <Logo />
+        </header>
+        <NavLinks />
+      </div>
+    </div>
+  </Wrapper>;
+};
+export default SmallSideBar;
