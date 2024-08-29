ce675963a4d5cab4af2f3a2aafb368775fbaf84f
Preet Patel <preetx.patel@mail.utoronto.ca>
2024-08-29 13:46:35 -0400

    Added middleware for validation of login, register, add job, and update user operations.

---
+++ middleware/validationMiddleware.js
@@ -0,0 +1,104 @@
+import { body, param, validationResult } from 'express-validator';
+import { BadRequestError, NotFoundError, UnauthorizedError } from '../errors/customErrors.js';
+import { JOB_STATUS, JOB_TYPE } from '../utils/constants.js';
+import mongoose from 'mongoose';
+import Job from '../models/JobModel.js';
+import User from '../models/UserModel.js';
+
+const withValidationErrors = (validateValues) => {
+  return [
+    validateValues,
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        const errorMessages = errors.array().map((error) => error.msg);
+        if (errorMessages[0].startsWith('No job')) {
+          throw new NotFoundError(errorMessages);
+        }
+        if (errorMessages[0].startsWith('Unauthorized to')) {
+          throw new UnauthorizedError('Unauthorized to access this route!');
+        }
+        throw new BadRequestError(errorMessages);
+      }
+      next();
+    },
+  ];
+};
+
+// Job Input Validator
+export const validateJobInput = withValidationErrors([
+  body('company').notEmpty().withMessage('Company is required!'),
+  body('position').notEmpty().withMessage('Position is required!'),
+  body('jobLocation').notEmpty().withMessage('Job Location is required!'),
+  body('jobStatus')
+    .isIn(Object.values(JOB_STATUS))
+    .withMessage('Invalid status value!'),
+  body('jobType')
+    .isIn(Object.values(JOB_TYPE))
+    .withMessage('Invalid type value!'),
+]);
+
+// Validate requests containing ID params
+export const validateIdParam = withValidationErrors([
+  param('id').custom(async (value, {req}) => {
+    const isValidId = mongoose.Types.ObjectId.isValid(value);
+    if (!isValidId) throw new BadRequestError('Invalid MongoDB id!');
+    const job = await Job.findById(value);
+    if (!job) throw new NotFoundError(`No job exists with id: ${value}`);
+
+    const isAdmin = req.user.role === 'admin';
+    const isOwner = req.user.userId === job.createdBy.toString();
+    if (!isAdmin && !isOwner) throw new UnauthorizedError('Unauthorized to access this route!');
+  }),
+]);
+
+// Validate register input
+export const validateRegisterInput = withValidationErrors([
+  body('name').notEmpty().withMessage('Name is required!'),
+  body('email')
+    .notEmpty()
+    .withMessage('Email is required!')
+    .isEmail()
+    .withMessage('Invalid email format!')
+    .custom(async (email) => {
+      const user = await User.findOne({ email });
+      if (user) throw new BadRequestError('Email already exists!');
+    }),
+  body('password')
+    .notEmpty()
+    .withMessage('Password is required!')
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters in length!'),
+  body('location').notEmpty().withMessage('Location is required!'),
+  body('lastName').notEmpty().withMessage('Last name is required!'),
+]);
+
+// Validate login input
+export const validateLoginInput = withValidationErrors([
+  body('email')
+    .notEmpty()
+    .withMessage('Email is required!')
+    .isEmail()
+    .withMessage('Invalid email format!'),
+  body('password')
+    .notEmpty()
+    .withMessage('Password is required!'),
+]);
+
+// Validate update user input
+export const validateUpdateUserInput = withValidationErrors([
+  body('name').notEmpty().withMessage('Name is required!'),
+  body('email')
+    .notEmpty()
+    .withMessage('Email is required!')
+    .isEmail()
+    .withMessage('Invalid email format!')
+    .custom(async (email, { req }) => {
+      const user = await User.findOne({ email });
+      if (user && user._id.toString() !== req.user.userId) {
+        throw new BadRequestError('Email already exists!');
+      }
+    }),
+  body('location').notEmpty().withMessage('Location is required!'),
+  body('lastName').notEmpty().withMessage('Last name is required!'),
+])
