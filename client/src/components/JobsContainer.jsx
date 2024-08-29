047c6b9479a4f53999b51dda6dbb3d7c62c54f7d
Preet Patel <preetx.patel@mail.utoronto.ca>
2024-08-29 13:55:30 -0400

    Designed job related components.

---
+++ client/src/components/JobsContainer.jsx
@@ -0,0 +1,40 @@
+import Job from './Job';
+import Wrapper from '../assets/wrappers/JobsContainer';
+import { useAllJobsContext } from '../pages/AllJobs';
+import PageBtnContainer from './PageBtnContainer';
+
+const JobsContainer = () => {
+  const { data } = useAllJobsContext();
+  const { jobs, totalJobs, numOfPages } = data;
+
+  // Safeguard: If data or jobs is undefined, return early
+  if (!data || !data.jobs) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display...</h2>
+      </Wrapper>
+    );
+  }
+  
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display...</h2>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <h5> {totalJobs} job{jobs.length > 1 && 's'} found </h5>
+      <div className='jobs'>
+        {jobs.map((job) => {
+          return <Job key={job._id} {...job} />;
+        })}
+      </div>
+      {numOfPages > 1 && <PageBtnContainer /> }
+    </Wrapper>
+  );
+};
+
+export default JobsContainer;
